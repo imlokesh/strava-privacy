deaace12e0f59314f771b44a43da418b51a27c38
imlokesh <me@imlokesh.com>
2023-02-17 14:52:19 +0530

    Add webhook logic

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { Browser, chromium, Page } from 'playwright';
 import winston from 'winston';
 import * as fs from 'fs';
 import { Context } from 'vm';
+import { Command } from 'commander'
+import ngork from 'ngrok';
+import express from 'express';
+import axios from 'axios';
+import { randomUUID } from 'crypto';
+import formData from 'form-data';
 
 const logger = winston.createLogger({
     level: 'info',
@@ -15,11 +21,48 @@ const logger = winston.createLogger({
     ],
 });
 
-let browser: Browser, context: Context, page: Page, clientId: string, clientSecret: string;
+const program = new Command();
+
+program
+    .option("--strava-email", "Strave login email address", "")
+    .option("--strava-password", "Strava login password", "")
+    .option("--client-id", "Strava application client id", "")
+    .option("--client-secret", "Strava application client secret", "")
+    .option("--ngrok-auth", "Ngrok auth token", "")
+    .option("--port", "Port for webhook client", "")
+    .option("--rules", "Rules to be used to set privacy on activities. ", []);
+
+program.parse(process.argv);
+const options = program.opts();
+
+const stravaEmail = options.stravaEmail || process.env.STRAVA_EMAIL;
+const stravaPassword = options.stravaPassword || process.env.STRAVA_PASSWORD;
+
+const clientId = options.clientId || process.env.CLIENT_ID;
+const clientSecret = options.clientSecret || process.env.CLIENT_SECRET;
+
+if (!stravaEmail || !stravaPassword) {
+    logger.error('Please specify strava login details. Use -h for options');
+}
+
+if (!clientId || !clientSecret) {
+    logger.error('Please specify strava client id and client secret from https://www.strava.com/settings/api. Use -h for options');
+}
+
+const port: number = parseInt(options.port || process.env.PORT) || 8095;
+
+const ngrokAuth = options.ngrokAuth || process.env.NGROK_AUTH;
+
+if (!ngrokAuth) {
+    logger.info('No ngrok auth token was found. Webhook service will be limited. ')
+}
+
+let browser: Browser, context: Context, page: Page;
 
 const LOGIN_URL = 'https://www.strava.com/login';
 const DASHBOARD_URL = 'https://www.strava.com/dashboard';
-const BROWSER_DATA_FILE = "browser_data.json";
+const BROWSER_DATA_FILE = 'strava_browser.json';
+const WEBHOOK_PATH = '/strava-privacy-helper'
 
 async function ExitProgram() {
     logger.info('Exiting program. ');
@@ -67,9 +110,9 @@ async function LoginToStrava() {
         logger.info('Logging in.');
 
         await page.getByPlaceholder('Your Email').click();
-        await page.getByPlaceholder('Your Email').fill(process.env.STRAVA_EMAIL || 'test@gmail.com');
+        await page.getByPlaceholder('Your Email').fill(stravaEmail);
         await page.getByPlaceholder('Password').click();
-        await page.getByPlaceholder('Password').fill(process.env.STRAVA_PASSWORD || 'password');
+        await page.getByPlaceholder('Password').fill(stravaPassword);
         await page.getByLabel('Remember me').check();
 
         let loginResponsePromise = page.waitForResponse(res => res.url() == 'https://www.strava.com/session' && res.request().method() == 'POST')
@@ -94,18 +137,84 @@ async function LoginToStrava() {
     }
 }
 
-async function GetAppCredentials() {
-    await LoginToStrava();
+async function RegisterWebhook() {
 
-    await page.goto('https://www.strava.com/settings/api');
+    const app = express();
+    var verifyToken = randomUUID();
 
-    clientId = await page.locator('[class*=TableRow]').filter({ hasText: 'Client ID' }).getByText(/[0-9]+/).innerText();
+    app.use(express.json());
 
-    await page.getByRole('button', { name: 'Show' }).first().click();
+    app.get(WEBHOOK_PATH, (req, res) => {
+        if (req.query["hub.challenge"] && req.query["hub.verify_token"] && req.query["hub.verify_token"] == verifyToken) {
+            res.send({ "hub.challenge": req.query["hub.challenge"] })
+        }
+        else {
+            res.sendStatus(405)
+        };
+    });
 
-    clientSecret = await page.locator('[class*=TableRow]').filter({ hasText: 'Client Secret' }).locator('p').innerText();
+    app.post(WEBHOOK_PATH, (req, res) => {
+        var x = 22;
+    });
 
-    await delay(50000);
+    app.listen(port, () => {
+        logger.info(`Webhook server running at http://localhost:${port}`);
+    });
+
+    const url = await ngork.connect({ addr: port, authtoken: ngrokAuth });
+    logger.info(`Ngork url registered at ${url}`);
+
+    const form = new formData();
+    form.append('client_id', clientId);
+    form.append('client_secret', clientSecret);
+    form.append('callback_url', url + WEBHOOK_PATH);
+    form.append('verify_token', verifyToken);
+
+    try {
+        let res = await axios.post('https://www.strava.com/api/v3/push_subscriptions', form, { headers: form.getHeaders() })
+        if (res.data.id) {
+            logger.info(`Webhook registered with strava. ID = ${res.data.id}`);
+        }
+        else {
+            throw new Error(`Unknown response. ${JSON.stringify(res.data)}`);
+        }
+    } catch (error: any) {
+        logger.error(`Error registering webhook. ${error} ${JSON.stringify(error.response.data)}`);
+    }
 }
 
-GetAppCredentials().catch(async err => logger.error(`Error getting app credentials. ${err}`) && await ExitProgram());
+async function UnregisterWebhook() {
+
+    logger.info(`Checking existing webhook. `);
+
+    try {
+        let res = await axios.get(`https://www.strava.com/api/v3/push_subscriptions?client_id=${clientId}&client_secret=${clientSecret}`);
+        let id = res.data[0]?.id;
+        if (!id) {
+            logger.info('No existing webhook found. ');
+            //return;
+        }
+
+        logger.info(`Found existing webhook with id ${id}`);
+
+        logger.info(`Unregestering webhook ${id}.`);
+
+        try {
+            const form = new formData();
+            form.append('client_id', clientId);
+            form.append('client_secret', clientSecret);
+
+            let res = await axios.delete(`https://www.strava.com/api/v3/push_subscriptions/${id}?client_id=${clientId}&client_secret=${clientSecret}`);
+
+            logger.info(`Webhook unregistered succesfuly. `);
+        } catch (error: any) {
+            logger.error(`Error unregistering webhook. ${error} ${JSON.stringify(error.response.data)}`);
+        }
+
+    } catch (error: any) {
+        logger.error(`Error checking existing webhook. ${error}`);
+    }
+}
+
+RegisterWebhook().then(UnregisterWebhook);
+// GetAppCredentials().catch(err => logger.error(`Error getting app credentials ${err}`)).then(RegisterWebhook)
